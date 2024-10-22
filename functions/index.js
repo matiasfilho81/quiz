a19b5a965e4882ef237ace8097141dcad30ee16e
José Matias Lemes Filho <jose.diletta@upbrasil.com>
2024-10-22 19:31:03 -0300

    configuração auth firebase

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,9 @@
 import { app } from './firebase-config.js';
 
 console.log("Firebase inicializado com sucesso!", app);
+
+import { onRequest } from 'firebase-functions/v2/https';
+
+export const helloWorld = onRequest((req, res) => {
+  res.send("Hello from Firebase!");
+});
